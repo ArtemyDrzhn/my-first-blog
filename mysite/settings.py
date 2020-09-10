bfcf2028c2ed3a70013944158b1b0796723012a5
ArtemyDrzhn <artemdrzhn@gmail.com>
2020-09-10 22:25:42 +0300

    Changed the HTML for the site.

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'tm6++-m#l+my$dax!3u=b!d11bas0#*$6rfxym%ogxlto#d6vx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
